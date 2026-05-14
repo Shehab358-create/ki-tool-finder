75e9b2c17cf13684a8c48fc07887bcdbd39a0158
Shehab <rezk.shehab123@gmail.com>
2026-05-14 23:39:24 +0200

    fix burger menu

--- script.js
+++ script.js
@@ -736,3 +736,7 @@ function toggleMenu() {
   const navLinks = document.querySelector(".nav-links");
   navLinks.classList.toggle("active");
 }
+
+function toggleMenu() {
+  document.querySelector(".nav-links").classList.toggle("active");
+}
