7888143d76d137d28e0286a4ecf4516f50349ed5
Shehab <rezk.shehab123@gmail.com>
2026-05-14 22:31:55 +0200

    added dark mode

--- script.js
+++ script.js
@@ -644,3 +644,18 @@ setTimeout(() => {
     result.innerHTML = workflowHTML;
 }, 9000);
 }
+
+function toggleTheme(){
+
+    document.body.classList.toggle("dark-mode");
+
+    const button = document.querySelector(".theme-toggle");
+
+    if(document.body.classList.contains("dark-mode")){
+        button.innerHTML = "☀️";
+    }
+
+    else{
+        button.innerHTML = "🌙";
+    }
+}
