6c6818c6282c433a33d09286b7e33cffd68a2d98
Shehab <rezk.shehab123@gmail.com>
2026-05-14 23:06:33 +0200

    added scroll reveal animation

--- script.js
+++ script.js
@@ -703,3 +703,19 @@ function typeEffect() {
 }
 
 setInterval(typeEffect, 120);
+
+const revealElements = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  revealElements.forEach((element) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      element.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
